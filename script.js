f62752216cfbc6e25513781889125fe357fefe18
taibislamdipu <mailtaibislam@gmail.com>
2020-07-27 13:28:06 +0600

    calculator all code done

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function egeCase() {
+    let displayNumber = document.getElementById('display').value;
+    if (displayNumber == '+' || displayNumber == '-' || displayNumber == '*' || displayNumber == '/' || displayNumber == '%' || displayNumber == '=') {
+        alert('Give input first.');
+        document.getElementById('display').value = "";
+    }
+}
+
+const displayNumber = document.getElementById('display').value;
+
+// insert number
+function insert(number) {
+    document.getElementById('display').value = document.getElementById('display').value + number;
+}
+
+// calculate numbers
+function calculate() {
+    document.getElementById('display').value = eval(document.getElementById('display').value)
+
+}
+
+function clearAll() {
+    document.getElementById('display').value = "";
+}
